b644e915a4d985ed57dddf75433f655d91db8413
sivasankar-jayavel <sivasankar6104@gmail.com>
2023-07-21 21:39:06 +0530

    ABOUT

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Who I am",
   description1:
-    "My name is Sivasankar J. I'm a web designer and developer from Coimbatore, TamilNadu.",
+    "My name is Sivasankar J. I'm a web designer and Full Stack (MERN) developer from Coimbatore, TamilNadu.",
   description2:
     "I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude.  I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, React, React Native, Redux, Bootstrap, Material UI, MongoDB, Mysql, Aws, etc. Aspiring Full-Stack Web developer who is proficient in both Front-end & Back-end frameworks.Talented backend developer adept at contributing to a highly collaborative work environment,finding solutions and determining customer satisfaction.Designed and developed web applications across multiple API's,Third-party integrations and Database.Looking forward to showcasing and utilizing my current skillset and experience of working Projects ",
   image: 1,
